078bcac7cbdac33cbd4342c5057cd97760566107
Mantsurov Nikita <n_mancurov@mail.ru>
2025-09-08 13:12:57 +0500

    feature: Созданы миграция, модель и настроены связи между Task и User

---
+++ app/Models/Task.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'status',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'status' => TaskStatus::class,
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
