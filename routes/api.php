93e5b5e5c695b27d7a0bfbccabd28538aac529a1
Mantsurov Nikita <n_mancurov@mail.ru>
2025-09-08 13:12:21 +0500

    feature: Создан ресурс для обработки /tasks

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
+
+    Route::middleware('api_auth')->apiResource('tasks', TaskController::class);
 });
